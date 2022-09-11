7c9bd82445f196fca8d10438a90d534881811a8a
Sameer Ajay Harankhedkar <harankhedkarsameer@gmail.com>
2022-09-11 19:43:08 +0530

    updated

--- src/Component/HomePage/TopNavbar/TopNavbar.jsx
+++ src/Component/HomePage/TopNavbar/TopNavbar.jsx
@@ -8,7 +8,7 @@ import { Box, Flex, Image, Text, AspectRatio, Link, Img } from "@chakra-ui/react
 
     return (
      <div>
-        <Img style={{"width":"100%","margin":"0px"}} src="https://awesomescreenshot.s3.amazonaws.com/image/2796027/31810612-429722be01b1586c1125801e2f96e0fe.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20220828%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220828T065801Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=b832d20a2e1fdacc7ab2cd5497287754dd2149cbf41f3c16c78c6b76d3ddf461" />
+        <Img style={{"width":"100%","margin":"0px"}} src="https://drive.google.com/file/d/1iD-qhBuxZKnaBEE34MEroJeKMLxqqWMR/view?usp=sharing" />
      </div>
           
     )
